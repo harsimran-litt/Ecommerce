a697898af19f6eb6477e932a9a174e716ef204a7
Harsimran Singh <Harsimransingh7396@conestogac.on.ca>
2024-06-11 18:11:35 -0400

    Add project files.

---
+++ Ecommerce/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce
+{
+    internal class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    
+    public Product(int productID, string productName, decimal price, int stock)
+    {
+        ProductID = productID;
+        ProductName = productName;
+        Price = price;
+        Stock = stock;
+    }
+    public void IncreaseStock(int amount)
+    {
+        if (amount < 0) throw new ArgumentException("Amount cannot be negative");
+        Stock += amount;
+    }
+
+    public void DecreaseStock(int amount)
+    {
+        if (amount < 0) throw new ArgumentException("Amount cannot be negative");
+        if (Stock - amount < 0) throw new InvalidOperationException("Not enough stock");
+        Stock -= amount;
+    }
+}
+}
